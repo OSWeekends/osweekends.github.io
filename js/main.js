1b576e0a9789322e6d84693b2028a5c0aae74284
Ulises Gascón <UlisesGascon@users.noreply.github.com>
2018-05-19 08:16:27 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 
 const agendaBtn = document.querySelector(".agendaBtn") ;
 const showDate  = new Date("Fri May 18 2018 00:00:00 GMT+0200 (CEST)") ;
-const hideDate  = new Date("Sat May 18 2018 23:59:59 GMT+0200 (CEST)") ;
+const hideDate  = new Date("Sat May 19 2018 23:59:59 GMT+0200 (CEST)") ;
 
 window.setInterval( function()  {
 
