b0a4d6d36f730b347310e3404afa956fe96af7a4
jorgeural <jorgeural88@gmail.com>
2018-10-11 19:17:46 +0200

    Issue 29: Add icons to blocks 2 and 3

--- src/components/HomeFeatures/HomeFeaturesCard.vue
+++ src/components/HomeFeatures/HomeFeaturesCard.vue
@@ -1,11 +1,6 @@
 <template>
   <div class="feature-card">
-    <v-icon
-      color="indigo"
-      size="80px"
-      class="feature-card__icon">
-      {{ icon }}
-    </v-icon>
+    <i class="feature-card__icon material-icons">  {{ icon }} </i>
     <h3 class="feature-card__title">{{ title }}</h3>
     <p class="feature-card__text"> {{ text }}</p>
   </div>
@@ -17,7 +12,7 @@ export default {
     icon: {
       type: String,
       required: true,
-      default: 'far fa-sticky-note'
+      default: 'worknpm'
       },
     text: {
       type: String,
@@ -47,8 +42,12 @@ export default {
     transform translateY(-10px) scale(1.03)
     box-shadow 0 15px 40px rgba(#000, .25)
 
+  &__icon
+    font-size 72px
+    color #333
+
   &__title
-    color #003ea5
+    color #333
     text-transform uppercase
     margin-bottom 15px
   
--- src/components/HomeProjects/Index.vue
+++ src/components/HomeProjects/Index.vue
@@ -1,11 +1,22 @@
 <template>
   <v-container class="home-projects">
-    <div
-      class="home-projects__title-row">
-      <v-icon>far fa-sticky-note</v-icon>
-      <h2 class="home-projects__title">Proyectos</h2>
-      <h3 class="home-projects__title2">Proyectos que se han hecho en la comunidad: </h3>
-    </div>
+    <v-layout
+      row
+      wrap
+      class="home-projects__title-row text-xs-center text-sm-left">
+      <v-flex 
+        xs12
+        sm1
+        class="home-projects__icon-wrapper" >
+        <i class="home-projects__icon material-icons"> code </i>
+      </v-flex>
+      <v-flex
+        xs12
+        sm11>
+        <h2 class="home-projects__title">Proyectos</h2>
+        <h3 class="home-projects__subtitle">Proyectos que se han hecho en la comunidad: </h3>
+      </v-flex>
+    </v-layout>
     <v-layout
       row
       wrap>
@@ -68,13 +79,20 @@ import HomeProjectsCard from '~/components/HomeProjects/HomeProjectsCard.vue'
 <style lang="stylus" scoped>
 
 .home-projects
+
   &__title-row
     padding-left 20px
     margin-bottom 40px
-
-  &__title
-  &__title2
     color #fff
+
+  &__icon-wrapper
+    display flex
+    align-items  center
+    justify-content center
+
+  &__icon
+    font-size 72px
+
 
   &__title
     font-size 40px
