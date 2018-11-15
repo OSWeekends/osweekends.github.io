932cd52412a4896bf01dbe8d4060a374d86ba326
Jorge Baumann <jorge.baumann.aguilar@gmail.com>
2018-11-15 18:33:43 +0100

    Host ENV

--- src/server/index.js
+++ src/server/index.js
@@ -3,7 +3,7 @@ const express = require('express')
 const consola = require('consola')
 const { Nuxt, Builder } = require('nuxt')
 const app = express()
-const host = process.env.HOST || '127.0.0.1'
+const host = process.env.HOST || '0.0.0.0'
 const port = process.env.PORT || 3000
 
 app.set('port', port)
